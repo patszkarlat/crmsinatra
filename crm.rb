6a02aabae74ec49604a0ff9a9e648ad5d4778e62
patszkarlat <punkgeek@mac.com>
2014-06-10 15:05:00 -0400

    forgot something

--- crm.rb
+++ crm.rb
@@ -6,14 +6,17 @@ get '/' do
 	erb :index
 end
 
-get 'contacts/:id' do
-	@contacts = []
+get 'contacts' do
+@contacts = []
 	@contacts << Contact.new("Pat", "Szkarlat", "punkgeek@mac.com", "Creative, Apple Inc.")
 	@contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-founder and Nice Guy")
 	@contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Lead Instructor of Awesomeness")
 	@contacts << Contact.new("Erik", "Dohnberg", "erik@bitmakerlabs.com", "Admissions Officer and Commiserator of Apple Employees")
 
 	erb :contacts
+end
+
+get 'contacts/:id' do
 end
 
 get '/contacts/new' do
