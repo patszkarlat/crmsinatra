b5be6b5bf41d7eab735804e2cc121f043dc6a094
patszkarlat <punkgeek@mac.com>
2014-06-10 14:57:23 -0400

    commit commit commit

--- crm.rb
+++ crm.rb
@@ -7,6 +7,11 @@ get '/' do
 end
 
 get 'contacts/:id' do
+	@contacts = []
+	@contacts << Contact.new("Pat", "Szkarlat", "punkgeek@mac.com", "Creative, Apple Inc.")
+	@contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-founder and Nice Guy")
+	@contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Lead Instructor of Awesomeness")
+	@contacts << Contact.new("Erik", "Dohnberg", "erik@bitmakerlabs.com", "Admissions Officer and Commiserator of Apple Employees")
 end
 
 get '/contacts/new' do
