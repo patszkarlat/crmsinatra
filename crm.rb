c98c9dcfd244a9bc3f06f449315861d88b699c6c
patszkarlat <punkgeek@mac.com>
2014-06-10 14:58:12 -0400

    holler

--- crm.rb
+++ crm.rb
@@ -12,6 +12,8 @@ get 'contacts/:id' do
 	@contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-founder and Nice Guy")
 	@contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Lead Instructor of Awesomeness")
 	@contacts << Contact.new("Erik", "Dohnberg", "erik@bitmakerlabs.com", "Admissions Officer and Commiserator of Apple Employees")
+
+	erb :contacts
 end
 
 get '/contacts/new' do
