3c7e14304e16e5b86effc41f6e371cb97cbc60f7
patszkarlat <punkgeek@mac.com>
2014-06-12 15:53:48 -0400

    added error message

--- crm.rb
+++ crm.rb
@@ -32,7 +32,11 @@ end
 
 get '/contacts/:id' do
 	@contact = @@rolodex.find(params[:id].to_i)
-	erb :show_contact
+	if @contact
+		erb :show_contact
+	else
+		raise Sinatra::NotFound
+	end
 end
 
 get 'contacts/:id/edit' do
